dae2ac2b979ad2d23d94c01bd3df94f6ac8d8146
João Gabriel Capitaneo <joaocapitaneo@gmail.com>
2020-12-14 00:35:36 -0300

    foi

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.java
@@ -7,8 +7,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
    private EditText editPrecoAlcool,editPrecoGasolina;
    private TextView textResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +23,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
    public void calcularPreco(){
-    String precoAlcool= editPrecoAlcool.getText().toString();
-    String precoGasolina= editPrecoGasolina.getText().toString();
-    precoAlcool="1";
-    precoGasolina="1";
-    int valorResultado= stringParaInt(precoAlcool)/stringParaInt(precoGasolina);
-       System.out.println(valorResultado);
+
+        String precoAlcool= editPrecoAlcool.getText().toString();
+        String precoGasolina= editPrecoGasolina.getText().toString();
+        precoAlcool="1";
+        precoGasolina="1";
+        boolean precoAlcoolValidado, precoGasolinaValidado;
+        precoAlcoolValidado= validarOtextInputLayout(precoAlcool);
+        precoGasolinaValidado= validarOtextInputLayout(precoGasolina);
+
+        if(precoAlcoolValidado==false) {
+            editPrecoAlcool.setError("este campo não pode estar vazio");
+        }
+        else if(precoGasolinaValidado==false){
+            editPrecoGasolina.setError("este campo não pode estar vazio");
+        }
+        else {
+
+            switch ( calculoAlcoolOuGasolina(precoAlcool, precoGasolina) ){
+                case  1:
+                    textResultado.setText("Bah dai melhor usar Álcool CPX");
+                break;
+                case 2:
+                    textResultado.setText("Melhor usar Gasolina");
+                break;
+                default:
+                    textResultado.setText("Erro interno.")
+                break;
+            }
+        }
+    }
+    public int calculoAlcoolOuGasolina(String alcool, String gasolina){
+        int valorResultado = stringParaInt(alcool) / stringParaInt(gasolina);
+        if (valorResultado >= 0.7){
+            return 2;
+        }
+        else return 1;
+    }
+    public boolean validarOtextInputLayout(String valorNoInput){
+        if(valorNoInput==""||valorNoInput==null){
+            //tratar como um erro do usuário.
+            return true;
+        }
+        else return false;
     }
     public int stringParaInt(String stringParan){
+        //CONVERTE INT PARA STRING ONDE DEVE SER USADO NO validarOtextInputLayout(String valorNoInput)
         Integer intParan= Integer.parseInt(stringParan);
         return intParan;
     }
