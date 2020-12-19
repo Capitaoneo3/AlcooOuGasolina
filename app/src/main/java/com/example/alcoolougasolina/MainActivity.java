ebe3e3c1d426d4a9c655753729fdad0d5808fbaf
João Gabriel Capitaneo <joaocapitaneo@gmail.com>
2020-12-19 14:48:59 -0300

    o luis me ajudo

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
    private EditText editPrecoAlcool,editPrecoGasolina;
    private TextView textResultado;
+   public Button CalcButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,56 +23,88 @@ public class MainActivity extends AppCompatActivity {
          editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
          editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
          textResultado= findViewById(R.id.textResultado);
+         CalcButton = findViewById(R.id.CalcButton);
 
+         CalcButton.setOnClickListener(v -> {
+              calcularPreco();
+         } );
+
+
+       // findViewById(R.id.CalcButton).setOnClickListener(actionHandle);
     }
+  /*  View.OnClickListener actionHandle= new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.CalcButton:
+                    calcularPreco();
+                    break;
 
-   public void calcularPreco(){
-
-        String precoAlcool= editPrecoAlcool.getText().toString();
-        String precoGasolina= editPrecoGasolina.getText().toString();
-
-        boolean precoAlcoolValidado, precoGasolinaValidado;
-        precoAlcoolValidado= validarOtextInputLayout(precoAlcool);
-        precoGasolinaValidado= validarOtextInputLayout(precoGasolina);
-
-        if(precoAlcoolValidado==false) {
-            editPrecoAlcool.setError("este campo não pode estar vazio");
-        }
-        else if(precoGasolinaValidado==false){
-            editPrecoGasolina.setError("este campo não pode estar vazio");
-        }
-        else {
-
-            switch ( calculoAlcoolOuGasolina(precoAlcool, precoGasolina) ){
-                case  1:
-                    textResultado.setText("Bah dai melhor usar Álcool CPX");
-                break;
-                case 2:
-                    textResultado.setText("Melhor usar Gasolina");
-                break;
-                default:
-                    textResultado.setText("Erro interno.");
-                break;
+          *//*  case R.id.b_2:
+                // act!
+                break;*//*
             }
         }
-    }
-    public int calculoAlcoolOuGasolina(String alcool, String gasolina){
-        int valorResultado = stringParaInt(alcool) / stringParaInt(gasolina);
-        if (valorResultado >= 0.7){
-            return 2;
+    };*/
+
+   public void calcularPreco(){//1  FRIST
+
+    String precoAlcool;
+    String precoGasolina;
+    precoAlcool = editPrecoAlcool.getText().toString();
+    precoGasolina = editPrecoGasolina.getText().toString();
+
+      // Toast.makeText(this, precoGasolina, Toast.LENGTH_SHORT).show();
+        if(precoAlcool.isEmpty()){
+            editPrecoAlcool.setError("campo Vazio");
+            return;
         }
-        else return 1;
+        if (precoGasolina.isEmpty()){
+            editPrecoGasolina.setError("campo vazio");
+            return;
+        }
+
+
+
+        switch ( calculoAlcoolOuGasolina(precoAlcool, precoGasolina) ){
+            case  1:
+                textResultado.setText("Bah dai melhor usar Álcool CPX");
+            break;
+            case 2:
+                textResultado.setText("Melhor usar Gasolina");
+            break;
+
+            default:
+                textResultado.setText("Erro interno.");
+            break;
+        }
+
+        editPrecoGasolina.setText("");
+        editPrecoGasolina.setText("");
+
     }
-    public boolean validarOtextInputLayout(String valorNoInput){
-        if(valorNoInput==""||valorNoInput==null){
+
+
+    public boolean validarOtextInputLayout(String valorNoInput){//2
+        if(valorNoInput.equals("")||valorNoInput==null){
             //tratar como um erro do usuário.
             return true;
         }
+
         else return false;
     }
-    public int stringParaInt(String stringParan){
+
+    public int calculoAlcoolOuGasolina(String alcool, String gasolina){//3
+        int valorResultado = stringParaInt(alcool) / stringParaInt(gasolina);
+        if (valorResultado >= 0.7){
+            return 2;//melhor usar gasolina
+        }
+        else return 1;
+    }
+
+    public int stringParaInt(String stringParan){//4
         //CONVERTE INT PARA STRING ONDE DEVE SER USADO NO validarOtextInputLayout(String valorNoInput)
-        Integer intParan= Integer.parseInt(stringParan);
+        int intParan= Integer.parseInt(stringParan);
         return intParan;
     }
-}// álcool/gasolina >= 0.7 : melhor usar gasolina.
+}                                               // álcool/gasolina >= 0.7 : melhor usar gasolina.
